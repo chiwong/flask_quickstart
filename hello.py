d55e00aec75ec77d4f0c8aee622f995a77cf5392
chi-wong <wong.169@gmail.com>
2013-02-24 00:19:42 +0000

    added routes for / and /hello

--- hello.py
+++ hello.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return 'This is the index page!'
+
+@app.route('/hello')
 def hello_world():
-	return 'Hello World!'
+	return 'Hello World!!'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=10080)
